e0cf563539873f5ebe0fcb4edbc89a6f550b33c9
AylinTravnik <aylin.travnik@gmail.com>
2021-11-05 21:08:22 -0300

    funcion cargarJuegos iniciada

--- programaZuccatoTravnik.php
+++ programaZuccatoTravnik.php
@@ -5,8 +5,8 @@ include_once("tateti.php");
 /***** DATOS DE LOS INTEGRANTES *******/
 /**************************************/
 
-/* Apellido, Nombre. Legajo. Carrera. mail. Usuario Github */
-/* ... COMPLETAR ... */
+/* Travnik Armitano, Valeria Aylin. FAI 3522 . Diplomatura Universitaria en diseño web. mail: valeria.travnik@est.fi.uncoma.edu.ar - usuario github: AylinTravnik */
+/* Zuccato, Stefano. FAI 3517 - Diplomatura Universitaria en Diseño Web mail: stefano.zuccato@est.fi.uncoma.edu.ar - usuario github: zuccato3517 */
 
 
 
@@ -15,10 +15,36 @@ include_once("tateti.php");
 /**************************************/
 /***** DEFINICION DE FUNCIONES ********/
 /**************************************/
+ function cargarJuegos(){
+
+ $juegos = array();
+
+echo "Elija la cantidad de juegos que desea ingresar: ";
+$cantidadJuegos = trim(fgets(STDIN));
+
+for ($numeroJuego=0; $numeroJuego<$cantidadJuegos; $numeroJuego++) {
+    echo "¿Que jugador jugo con X?";
+    $juegos[$numeroJuego]["X"] = trim(fgets(STDIN));
+    echo "¿Que jugador jugo con O?";
+    $juegos[$numeroJuego]["O"] = trim(fgets(STDIN));
+    echo "¿Como concluyo el juego? (Ganador/Empate)";
+    $juegos[$numeroJuego]["resultado"] = trim(fgets(STDIN));
+    echo "¿que puntaje obtuvo X?";
+    $juegos[$numeroJuego]["Ptos X"] = trim(fgets(STDIN));
+    echo "¿que puntaje obtuvo O?";
+    $juegos[$numeroJuego]["Ptos O"] = trim(fgets(STDIN));
+}
+};
+ 
+/*
+for ($numeroJuego=0; $numeroJuego<$cantidadJuegos; $numeroJuego++) {
+    echo $juegos[$numeroJuego]["ganador"];
+    echo $juegos[$numeroJuego]["perdedor"];
+    echo $juegos[$numeroJuego]["puntaje"];
+};
 
 
-
-
+*/
 
 
 
